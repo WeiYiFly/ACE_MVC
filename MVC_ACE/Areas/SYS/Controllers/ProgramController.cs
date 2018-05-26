100ec424420b58a81963e1da1519db2bed7c0aed
yiwei <2665430543@qq.com>
2018-05-26 15:34:58 +0800

    環境配置

---
+++ MVC_ACE/Areas/SYS/Controllers/ProgramController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ACE.Areas.SYS.Controllers
+{
+    public class ProgramController : Controller
+    {
+        // GET: SYS/Program
+        public ActionResult Index()
+        {
+            ViewBag.Title = "";
+            return View();
+        }
+    }
+}
